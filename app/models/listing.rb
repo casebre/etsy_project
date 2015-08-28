aeb30d5db906099c23dbfe55cca4888e07e31a8d
Rafael Casale Abe <rafael.casaleabe@gmail.com>
2015-08-28 11:53:36 -0400

    Section 6 - Lecture 21: Finishing setup Dropbox.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,14 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, 
-		:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "me.jpg",
-		:storage => :dropbox,
-		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "me.jpg"
+		#remember to change me.jpg :/
+	else	
+		has_attached_file :image, 
+			:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "me.jpg",
+			:storage => :dropbox,
+			:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+			:path => ":style/:id_filename"
+	end
+
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
