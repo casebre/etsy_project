9e5a69622ce4915d4d143c955bb3bc9f4422b3c7
Rafael Casale Abe <rafael.casaleabe@gmail.com>
2015-08-30 19:31:52 -0400

    Customizing Devise Forms, adding alerts via Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# It will help me to define a better msg alert and style
+	def flash_class(level)
+		case level.to_sym
+		when :notice then "alert alert-info"
+		when :sucess then "alert alert-sucess"
+		when :warning then "alert alert-warning"
+		when :danger then "alert alert-danger"
+		end
+	end
 end
