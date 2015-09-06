a40e6a106c966ae243fd7dbbf8ca4596c08faa50
Rafael Casale Abe <rafael.casaleabe@gmail.com>
2015-09-06 17:32:08 -0400

    Adding a admin/seller page, using 'where' statement. Also the listing of products was ordered using 'order'syntax. Section 8 - Lecture 29

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get '/contact' => 'pages#contact'
 
+  get "seller" => "listings#seller"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
